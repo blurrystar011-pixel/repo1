81a3088732017f34627a9da47592a67a74ad5a1f
aditya143p <148969553+aditya143p@users.noreply.github.com>
2025-08-16 19:21:21 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000 ;
 
 app.use(express.json());
 app.use(cors());
